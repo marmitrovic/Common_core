2f340b197808bdd34f6465e5ed74525ac444cd8f
Marko Mitrovic <mmitrovi@c3r3s1.42madrid.com>
2026-09-22 18:24:01 +0200

    Save progress

---
+++ Piscine_reloaded/ex06/ft_print_alphabet.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmitrovi <mmitrovi@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/22 17:59:52 by mmitrovi          #+#    #+#             */
+/*   Updated: 2026/09/22 18:00:35 by mmitrovi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	write(1, "abcdefghijklmnopqrstuvwxyz", 26);
+}
+
+/*
+int main (void)
+{
+    ft_print_alphabet();
+
+    return 0;
+} */
