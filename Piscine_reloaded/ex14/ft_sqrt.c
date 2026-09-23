f00b4e037890c510b107f375bc6787d4b4d10ea0
marmitrovic <emarkomitrovic@gmail.com>
2026-09-23 10:30:36 +0200

    ex14

---
+++ Piscine_reloaded/ex14/ft_sqrt.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sqrt.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmitrovi <mmitrovi@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/23 10:29:19 by mmitrovi          #+#    #+#             */
+/*   Updated: 2026/09/23 10:29:25 by mmitrovi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_sqrt(int nb)
+{
+	int	i;
+
+	i = 1;
+	if (nb <= 0)
+		return (0);
+	while (i * i < nb)
+	{
+		i++;
+	}
+	if (i * i == nb)
+		return (i);
+	else
+		return (0);
+}
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("%d\n", ft_sqrt(25));
+	return (0);
+}*/
