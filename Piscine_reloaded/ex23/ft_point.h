e877db71e2fa146584c794a6c0b98bafce3c0193
Marko Mitrovic <mmitrovi@c3r4s3.42madrid.com>
2026-09-23 19:27:31 +0200

    last push

--- Piscine_reloaded/ex23/ft_point.h
+++ Piscine_reloaded/ex23/ft_point.h
@@ -6,17 +6,16 @@
 /*   By: mmitrovi <mmitrovi@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/23 13:20:25 by mmitrovi          #+#    #+#             */
-/*   Updated: 2026/09/23 13:29:25 by mmitrovi         ###   ########.fr       */
+/*   Updated: 2026/09/23 19:26:03 by mmitrovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_POINT_H
 # define FT_POINT_H
 
-# define FT_POINT_H
 typedef struct s_point
 {
-	int x;
-	int y;
+	int	x;
+	int	y;
 }	t_point;
 #endif
