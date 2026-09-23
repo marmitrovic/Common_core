b76781f0685eb1e9c1b571ef97e4f9a66015759f
marmitrovic <emarkomitrovic@gmail.com>
2026-09-23 13:37:14 +0200

    point header

--- Piscine_reloaded/ex23/ft_point.h
+++ Piscine_reloaded/ex23/ft_point.h
@@ -1,18 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_abs.h                                           :+:      :+:    :+:   */
+/*   ft_point.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmitrovi <mmitrovi@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/09/23 13:17:20 by mmitrovi          #+#    #+#             */
-/*   Updated: 2026/09/23 13:20:21 by mmitrovi         ###   ########.fr       */
+/*   Created: 2026/09/23 13:20:25 by mmitrovi          #+#    #+#             */
+/*   Updated: 2026/09/23 13:29:25 by mmitrovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_ABS_H
-# define FT_ABS_H
+#ifndef FT_POINT_H
+# define FT_POINT_H
 
-# define ABS(Value) (((Value) < 0) ? -(Value) : (Value))
-
+# define FT_POINT_H
+typedef struct s_point
+{
+	int x;
+	int y;
+}	t_point;
 #endif
