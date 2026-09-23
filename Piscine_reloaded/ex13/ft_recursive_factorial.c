5a80a16d91dbf51c9b686f978111c19d74397b0d
marmitrovic <emarkomitrovic@gmail.com>
2026-09-23 10:07:11 +0200

    ex13

--- Piscine_reloaded/ex13/ft_recursive_factorial.c
+++ Piscine_reloaded/ex13/ft_recursive_factorial.c
@@ -1,38 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmitrovi <mmitrovi@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/09/23 09:15:17 by mmitrovi          #+#    #+#             */
-/*   Updated: 2026/09/23 09:56:51 by mmitrovi         ###   ########.fr       */
+/*   Created: 2026/09/23 10:04:42 by mmitrovi          #+#    #+#             */
+/*   Updated: 2026/09/23 10:06:50 by mmitrovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	ft_iterative_factorial(int nb)
+int	ft_recursive_factorial(int nb)
 {
-	int	res;
-
-	res = 1;
+	if (nb == 0)
+		return (1);
 	if (nb < 0)
-	{
 		return (0);
-	}
-	while (nb != 0)
-	{
-		res *= nb;
-		nb--;
-	}
-	return (res);
+	return (nb * ft_recursive_factorial(nb - 1));
 }
 /*
 #include <stdio.h>
 
 int	main(void)
 {
-	printf("%d", ft_iterative_factorial(15));
+	printf("%d", ft_recursive_factorial(15));
+
 	return (0);
 }*/
+
+/*
+nb-- doesnt function because it is 
+returning old value in the recursion
+*/
