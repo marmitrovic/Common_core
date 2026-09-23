f3936fafb92f296023964cd5a911d10e7acd03ae
marmitrovic <emarkomitrovic@gmail.com>
2026-09-23 09:16:56 +0200

    ex12 done

--- Piscine_reloaded/ex12/ft_iterative_factorial.c
+++ Piscine_reloaded/ex12/ft_iterative_factorial.c
@@ -1,7 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmitrovi <mmitrovi@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/23 09:15:17 by mmitrovi          #+#    #+#             */
+/*   Updated: 2026/09/23 09:16:06 by mmitrovi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
 int	ft_iterative_factorial(int nb)
 {
-	
-	return ;
+	int	res;
+
+	res = 1;
+	while (nb != 0)
+	{
+		res *= nb;
+		nb--;
+	}
+	return (res);
 }
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("%d", ft_iterative_factorial(15));
+	return (0);
+}*/
